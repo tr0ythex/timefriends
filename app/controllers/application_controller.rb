2784cd54fe52487f87450d4e1511db04173ae1c8
Denis Shtabnoy <tr0ythex@gmail.com>
2015-12-30 22:05:20 +0000

    Finishes first production version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
   protect_from_forgery with: :null_session
-  # before_action :ensure_json_request  
-
-  # def ensure_json_request  
-  #   return if request.format == :json
-  #   render :nothing => true, :status => 406  
-  # end 
   
   private
   
@@ -19,6 +13,4 @@ class ApplicationController < ActionController::Base
   def authenticate_with_token!
     head :unauthorized unless current_user.present?
   end
-  
-  # helper_method :current_user
 end
