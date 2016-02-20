699a00b320c8e4d75642f492de18963cd21322ef
Denis Shtabnoy <tr0ythex@gmail.com>
2016-02-20 13:36:22 +0000

    Setting up capistrano

--- db/migrate/20160214121158_change_starttime_column_type_in_posts.rb
+++ db/migrate/20160214121158_change_starttime_column_type_in_posts.rb
@@ -1,5 +1,5 @@
 class ChangeStarttimeColumnTypeInPosts < ActiveRecord::Migration
   def change
-    change_column :posts, :start_time, :datetime
+    change_column :posts, :start_time, 'timestamp USING CAST(start_time AS timestamp)'
   end
 end
