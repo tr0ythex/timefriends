c90608e8c3c2d8cf44428ee9e5c0b9bdef705213
Denis Shtabnoy <tr0ythex@gmail.com>
2016-02-20 20:57:18 +0000

    Setting up capistrano

--- db/migrate/20160214121142_rename_datetime_and_add_end_time_to_posts.rb
+++ db/migrate/20160214121142_rename_datetime_and_add_end_time_to_posts.rb
@@ -1,6 +1,7 @@
 class RenameDatetimeAndAddEndTimeToPosts < ActiveRecord::Migration
   def change
     rename_column :posts, :datetime, :start_time
+    change_column :posts, :start_time, :datetime
     add_column :posts, :end_time, :datetime
   end
 end
