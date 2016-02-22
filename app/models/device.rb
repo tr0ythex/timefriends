f3623d55d29e94c2a57310f5845c361e524bbda5
Denis Shtabnoy <tr0ythex@gmail.com>
2016-02-22 14:29:07 +0000

    Reset to default

--- app/models/device.rb
+++
@@ -1,5 +0,0 @@
-class Device < ActiveRecord::Base
-  scope :ios, -> { where(device_type: 'ios') }
-  
-  
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222084717) do
+ActiveRecord::Schema.define(version: 20160215162323) do
 
   create_table "accessions", force: :cascade do |t|
     t.integer  "user_id"
@@ -55,13 +55,6 @@ ActiveRecord::Schema.define(version: 20160222084717) do
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
-
-  create_table "devices", force: :cascade do |t|
-    t.string   "registration_id"
-    t.string   "device_type"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
 
   create_table "friendships", force: :cascade do |t|
     t.integer  "friendable_id"
