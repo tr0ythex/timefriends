336e998fdb477ad27991a91d40e0799a95abe3f0
Denis Shtabnoy <tr0ythex@gmail.com>
2016-03-28 15:33:10 +0000

    Adds dependent destroy action on post joined users

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  has_many :joined_users, through: :accessions, class_name: 'User'
+  has_many :joined_users, through: :accessions, class_name: 'User',
+    dependent: :destroy
   has_many :accessions
   
   has_many :comments
