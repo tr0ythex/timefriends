674f7827758540d996672f17c6a860418198a16b
Denis Shtabnoy <tr0ythex@gmail.com>
2016-03-30 13:19:10 +0000

    Removes seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-10.times do |i|
-  user = User.create!(login: "user#{i+1}", email: "user#{i+1}@example.com", 
-       password: "password#{i+1}", hide_acc: i.odd? ? true : false, 
-       first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name)
-  # user.save
-  user.bg_packs.create({name: "pack1", device_type: "ipad"})
-  user.bg_packs.create({name: "pack1", device_type: "ipad2"})
-  user.bg_packs.create({name: "pack2", device_type: "ipad2"})
-end
+# 10.times do |i|
+#   user = User.create!(login: "user#{i+1}", email: "user#{i+1}@example.com", 
+#       password: "password#{i+1}", hide_acc: i.odd? ? true : false, 
+#       first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name)
+#   # user.save
+#   user.bg_packs.create({name: "pack1", device_type: "ipad"})
+#   user.bg_packs.create({name: "pack1", device_type: "ipad2"})
+#   user.bg_packs.create({name: "pack2", device_type: "ipad2"})
+# end
