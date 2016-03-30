3363f8e8a5b3e1fd98f5925b5c8f720799bf1a0c
Denis Shtabnoy <tr0ythex@gmail.com>
2016-03-30 14:01:53 +0000

    Casting datetime for postgres

--- db/migrate/20160214121142_rename_datetime_and_add_end_time_to_posts.rb
+++ db/migrate/20160214121142_rename_datetime_and_add_end_time_to_posts.rb
@@ -1,7 +1,8 @@
 class RenameDatetimeAndAddEndTimeToPosts < ActiveRecord::Migration
   def change
-    rename_column :posts, :datetime, :start_time
+    remove_column :posts, :datetime
     # change_column :posts, :start_time, 'datetime USING CAST(start_time AS datetime)'
+    add_column :posts, :start_time, :datetime
     add_column :posts, :end_time, :datetime
   end
 end
