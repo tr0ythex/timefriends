48b34ae347434fe9b60910353a976bb04f6cfe8c
Denis Shtabnoy <tr0ythex@gmail.com>
2016-04-03 14:59:17 +0000

    Changes alert message in controllers pushes

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -14,7 +14,7 @@ class Api::V1::CommentsController < ApplicationController
         p_user_pushes = []
         p_user.devices.each do |device|
           p_user_pushes << APNS::Notification.new(device.token, 
-              :alert => "Новый комментарий от пользователя #{comment.user.login} к '#{comment.post.body}'",
+              :alert => "#{I18n.t :new_comment_push, user: comment.user.login, post: comment.post.body}",
               :badge => 1, :sound => 'default', :other => {:sent => {:photo_url => comment.user.photo_url, :type => 1}})
         end
         # Send pushes to all user devices
